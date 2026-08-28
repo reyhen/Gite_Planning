8f72ee19a96c824e0e36d27bd56169e8cd108802
reyhen <rhenriet82@yahoo.com>
2026-08-28 21:03:17 +0200

    Update Program.fs

--- Program.fs
+++ Program.fs
@@ -21,57 +21,57 @@ module Program =
 
         Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(fun webHostBuilder ->
-                webHostBuilder
-                    .UseUrls(sprintf "http://0.0.0.0:%s" port)
+
+                // CONFIGURATION DES URLS
+                webHostBuilder.UseUrls(sprintf "http://0.0.0.0:%s" port) |> ignore
+
+                // CONFIGURATION DES SERVICES
+                webHostBuilder.ConfigureServices(fun services ->
+                    services
+                        .AddControllersWithViews()
+                        .AddRazorRuntimeCompilation()
                     |> ignore
 
-                webHostBuilder
-                    .Configure(fun app ->
-                        let env = app.ApplicationServices.GetRequiredService<IHostEnvironment>()
+                    services.AddRazorPages() |> ignore
 
-                        if not env.IsDevelopment() then
-                            app.UseExceptionHandler("/Home/Error") |> ignore
-
-                        app.UseStaticFiles() |> ignore
-                        app.UseRouting() |> ignore
-                        app.UseAuthorization() |> ignore
-
-                        app.UseEndpoints(fun endpoints ->
-                            endpoints.MapControllerRoute(
-                                name = "default",
-                                pattern = "{controller=Home}/{action=Index}/{id?}"
-                            )
-                            |> ignore
-
-                            endpoints.MapRazorPages() |> ignore
-                        )
-                    )
-                    |> ignore
-
-                webHostBuilder
-                    .ConfigureServices(fun services ->
-                        services
-                            .AddControllersWithViews()
-                            .AddRazorRuntimeCompilation()
-                        |> ignore
-
-                        services.AddRazorPages() |> ignore
-
-                        services
-                            .AddDataProtection()
-                            .PersistKeysToFileSystem(
-                                DirectoryInfo(
-                                    Path.Combine(
-                                        Directory.GetCurrentDirectory(),
-                                        "App_Data",
-                                        "DataProtectionKeys"
-                                    )
+                    services
+                        .AddDataProtection()
+                        .PersistKeysToFileSystem(
+                            DirectoryInfo(
+                                Path.Combine(
+                                    Directory.GetCurrentDirectory(),
+                                    "App_Data",
+                                    "DataProtectionKeys"
                                 )
                             )
-                            .SetApplicationName("Gite_Planning")
-                        |> ignore
-                    )
+                        )
+                        .SetApplicationName("Gite_Planning")
                     |> ignore
+                ) |> ignore
+
+                // CONFIGURATION DU PIPELINE HTTP
+                webHostBuilder.Configure(fun app ->
+                    let env = app.ApplicationServices.GetRequiredService<IHostEnvironment>()
+
+                    if not env.IsDevelopment() then
+                        app.UseExceptionHandler("/Home/Error") |> ignore
+
+                    // NE PAS utiliser HTTPS sur Render
+                    // app.UseHttpsRedirection() |> ignore
+
+                    app.UseStaticFiles() |> ignore
+                    app.UseRouting() |> ignore
+                    app.UseAuthorization() |> ignore
+
+                    app.UseEndpoints(fun endpoints ->
+                        endpoints.MapControllerRoute(
+                            name = "default",
+                            pattern = "{controller=Home}/{action=Index}/{id?}"
+                        ) |> ignore
+
+                        endpoints.MapRazorPages() |> ignore
+                    )
+                ) |> ignore
             )
             .Build()
             .Run()
